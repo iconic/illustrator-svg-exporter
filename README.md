71d1c0a4a8d4e56271e28086dff61165f26b9e00
somerandomdude <pj@somerandomdude.com>
2015-01-14 13:13:38 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Illustrator SVG Exporter
+
+Exporting SVGs from Illustrator is a slow, laborious process&mdash;this script fixes that. The script is doesn't waste your time with GUI or settings you'll ever use. You just run the script, select a location to export and you have your SVGs. We love the concept behind [Generator](http://blogs.adobe.com/photoshopdotcom/2013/09/introducing-adobe-generator-for-photoshop-cc.html) and this script takes a strong cue from it. The script exports any layer, group or path named with the `.svg` extension.
+
+* [Installation](#installation)
+* [Running the Script](#running-the-script)
+  * [Document Setup](#document-setup)
+  * [Details](#details)
+
+<a name="installation"></a>
+## Installation
+
+You don't _have_ to install the script to use it (more on that later), but installing the script is by far the best way to use it. All you need to do is drop the `SVG Exporter.jsx` file in one of the following directories:
+
+Windows: `C:\Program Files\Adobe\Adobe lllustratorCC2014\Presets\[language]\Scripts\`
+Mac OS: `/Applications/Adobe lllustrator CC 2014/Presets/[language]/Scripts/`
+
+Note: Make sure to restart Illustrator if you installed the script while the Application is running.
+
+<a name="running-the-script"></a>
+### Running the Script
+
+Once the script is installed, you'll be able to run it by going to `File > Scripts > SVG Exporter`. As mentioned, you don't need to install the script. If you want to run it as a one-off, select `File > Scripts > Other Script...` and select the `SVG Exporter.jsx` file in the file chooser.
+
+Once you run the script, you'll be prompted to select a location to save the SVG files. After a location is set, you're done&mdash;the script does the rest.
+
+<a name="document-setup"></a>
+### Document Setup
+
+The script doesn't force any setup or organization on you. You can export layers, groups, compound paths or individual paths. Just name the path/layer/group/compound path what you want the file name to be (e.g., my-cool-vector-drawing.svg) and the script will prep it for export. You can export nested layers (example: export indiviual assets as well all assets in a parent layer). If you previously named an element for export but now don't want to export for some reason, simply lock the element.
---
+++ SVG Exporter.jsx
@@ -0,0 +1,229 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Waybury
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
+#target illustrator
+
+var exportFolder,
+    sourceDoc,
+    itemsToExport,
+    exportDoc,
+    svgOptions;
+
+try {
+  if ( app.documents.length > 0 ) {
+    svgOptions = new ExportOptionsSVG();
+    svgOptions.artBoardClipping = true;
+    svgOptions.embedRasterImages = false;
+    svgOptions.cssProperties = SVGCSSPropertyLocation.PRESENTATIONATTRIBUTES;
+    svgOptions.fontSubsetting = SVGFontSubsetting.None;
+    svgOptions.documentEncoding = SVGDocumentEncoding.UTF8;
+    svgOptions.coordinatePrecision = 4;
+
+    itemsToExport = [];
+    sourceDoc = app.activeDocument;
+    exportFolder = Folder.selectDialog('Select Folder to Save Files');
+    exportDoc = documents.add(DocumentColorSpace.RGB);
+
+    main();
+
+    exportDoc.close(SaveOptions.DONOTSAVECHANGES);
+  }
+  else{
+    throw new Error('There are no documents open. Open a document and try again.');
+  }
+}
+catch(e) {
+  alert(e.message, "Script Alert", true);
+}
+
+function main() {
+  var item;
+
+  itemsToExport = getNamedItems(sourceDoc);
+
+  for ( var i = 0, len = itemsToExport.length; i < len; i++ ) {
+
+    item = itemsToExport[i];
+
+    if ( item.typename === 'Layer' ) {
+      exportLayer(item);
+    } else {
+      exportItem(item);
+    }
+
+    // Empty export document
+    while ( exportDoc.layers[0].pageItems.length ) {
+      exportDoc.layers[0].pageItems[0].remove();
+    }
+  }
+}
+
+function exportLayer(layer) {
+
+  var item,
+      startX,
+      startY,
+      endX,
+      endY,
+      name,
+      layerItems;
+
+  layerItems = [];
+
+  for ( var i = 0, len = layer.pageItems.length; i < len; i++ ) {
+    layerItems.push(layer.pageItems[i]);
+  }
+  recurseItems(layer.layers, layerItems);
+
+  if ( !layerItems.length ) {
+    return;
+  }
+
+  name = layer.name;
+
+  for ( i = 0, len = layerItems.length; i < len; i++ ) {
+    app.activeDocument = sourceDoc;
+    item = layerItems[i];
+    item.duplicate( exportDoc, ElementPlacement.PLACEATEND );
+  }
+
+  app.activeDocument = exportDoc;
+
+  for ( i = 0, len = exportDoc.pageItems.length; i < len; i++) {
+
+    item = exportDoc.pageItems[i];
+
+    /*
+     * We want the smallest startX, startY for obvious reasons.
+     * We also want the smallest endX and endY because Illustrator
+     * Extendscript treats this coordinate reversed to how the UI
+     * treats it (e.g., -142 in the UI is 142).
+     *
+     */
+    startX = ( !startX || startX > item.visibleBounds[0] ) ? item.visibleBounds[0] : startX;
+    startY = ( !startY || startY < item.visibleBounds[1] ) ? item.visibleBounds[1] : startY;
+    endX = ( !endX || endX < item.visibleBounds[2] ) ? item.visibleBounds[2] : endX;
+    endY = ( !endY || endY > item.visibleBounds[3] ) ? item.visibleBounds[3] : endY;
+  }
+
+  exportSVG( exportDoc, name, [startX, startY, endX, endY], svgOptions );
+}
+
+function exportItem(item) {
+
+  var name;
+
+  name = item.name;
+  item.duplicate( exportDoc, ElementPlacement.PLACEATEND );
+
+  app.activeDocument = exportDoc;
+
+  exportSVG( exportDoc, name, item.visibleBounds, svgOptions );
+}
+
+function exportSVG(doc, name, bounds, exportOptions) {
+  doc.layers[0].name = name.slice(0, -4);
+  doc.artboards[0].artboardRect = bounds;
+
+  var file = new File( exportFolder.fsName + '/' + name );
+  doc.exportFile( file, ExportType.SVG, exportOptions );
+}
+
+function getNamedItems(doc) {
+  var item,
+      items,
+      doclayers;
+
+  items = [];
+
+  // Check all pageItems for name match
+  for ( var i = 0, len = doc.pageItems.length; i < len; i++ ) {
+    item =  doc.pageItems[i];
+
+    if ( item.name.split('.').pop() === 'svg' && !item.locked && !anyParentLocked(item) ) {
+      items.push(item);
+    }
+  }
+
+  // Check all layers for name match
+  doclayers = [];
+  recurseLayers( doc.layers, doclayers );
+
+  for ( i = 0, len = doclayers.length; i < len; i++ ) {
+    item = doclayers[i];
+
+    if ( item.name.split('.').pop() === 'svg' && !item.locked && !anyParentLocked(item) ) {
+      items.push(item);
+    }
+  }
+
+  return items;
+}
+
+function recurseLayers(layers, layerArray) {
+
+  var layer;
+
+  for ( var i = 0, len = layers.length; i < len; i++ ) {
+    layer = layers[i];
+    if ( !layer.locked ) {
+      layerArray.push(layer);
+    }
+    if (layer.layers.length > 0) {
+      recurseLayers( layer.layers, layerArray );
+    }
+  }
+}
+
+function recurseItems(layers, items) {
+
+  var layer;
+
+  for ( var i = 0, len = layers.length; i < len; i++ ) {
+    layer = layers[i];
+    if ( layer.pageItems.length > 0 && !layer.locked ) {
+      for ( var j = 0, plen = layer.pageItems.length; j < plen; j++ ) {
+        if ( !layer.pageItems[j].locked ) {
+          items.push(layer.pageItems[j]);
+        }
+      }
+    }
+
+    if ( layer.layers.length > 0 ) {
+      recurseItems( layer.layers, items );
+    }
+  }
+}
+
+function anyParentLocked(item) {
+  while ( item.parent ) {
+    if ( item.parent.locked ) {
+      return true;
+    }
+    item = item.parent;
+  }
+
+  return false;
+}
